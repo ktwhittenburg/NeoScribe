bba7dc94fa86f8f6f1842223279c1d4a356e8d71
ktwhittenburg <knox@skinny.org>
2017-04-06 16:53:16 -0500

    Create signup function and define error methods

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'users/new'
 
   root 'static_pages#home'
+  get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
