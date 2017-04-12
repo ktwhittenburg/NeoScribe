7c9691fb9cdf89d8e7889459ad45f2a08473e60a
ktwhittenburg <knox@skinny.org>
2017-04-12 14:23:38 -0500

    Configure help route and add link

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
   get  '/signup',  to: 'users#new'
+  get  '/help', to: 'static_pages#help'
   post '/signup',  to: 'users#create'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
