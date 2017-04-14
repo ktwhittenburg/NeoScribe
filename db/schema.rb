82a2d6ce2b23400145c7eb49305b11f1cdbafe25
ktwhittenburg <knox@skinny.org>
2017-04-14 13:29:09 -0500

    Migrate projects model and fix about_path

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170404002533) do
+ActiveRecord::Schema.define(version: 20170414174535) do
+
+  create_table "projects", force: :cascade do |t|
+    t.text     "title"
+    t.text     "synopsis"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_projects_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
