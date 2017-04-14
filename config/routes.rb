c7fcb9d3d6ed09be52ce2dc05ca7b1568a080fce
Brian E. Sippel <besippel@knox.edu>
2017-04-14 13:14:38 -0500

    added about page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
   get  '/signup',  to: 'users#new'
+  get '/about', to: 'static_pages#about'
   post '/signup',  to: 'users#create'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
