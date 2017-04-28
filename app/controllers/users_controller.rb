a56d69dc9ce52ce73a8c3e05bcf93265e762c282
ktwhittenburg <knox@skinny.org>
2017-04-28 09:05:55 -0500

    Fix crash when viewing user profile while not logged in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user
 
   def show
     @user = User.find(params[:id])
@@ -25,6 +26,12 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+	
+	# Confirms the correct user.
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 	
 end
