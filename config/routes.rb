e59ed44d8183b6dc7a321e1c7b3b3dc4e54fd835
ktwhittenburg <knox@skinny.org>
2017-05-01 09:43:37 -0500

    Small route fix

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   resources :users do
 	resources :projects do
 	  resources :trials
+	end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
