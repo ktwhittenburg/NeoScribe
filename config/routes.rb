4c2d98e2bb1cd24aa2a7b5be741086afa9819848
ktwhittenburg <knox@skinny.org>
2017-05-04 10:52:24 -0500

    Refactor routing

--- config/routes.rb
+++ config/routes.rb
@@ -13,9 +13,16 @@ Rails.application.routes.draw do
   patch '/users/:user_id/projects/:id/edit' => 'projects#update'
   put '/users/:user_id/projects/:id/edit' => 'projects#update'
   resources :users do
-	resources :projects do
-	  resources :trials
-	end
+	resources :projects, :name_prefix => "user_"
   end
+  resources :projects do
+    resources :trials, :name_prefix => "project_"
+  end
+  resources :trials
+  #resources :users do
+	#resources :projects do
+	  #resources :trials
+	#end
+  #end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
